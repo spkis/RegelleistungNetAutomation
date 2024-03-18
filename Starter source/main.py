3fe6badee3b77bb2306bfbc2d25285003bdb91e0
Tomas Neubauer <tomas+juniz@quix.io>
2024-03-18 12:54:02 +0000

    Edited 'main.py'

--- Starter source/main.py
+++ Starter source/main.py
@@ -3,6 +3,10 @@ import requests
 from datetime import datetime, timedelta
 import io  # For handling in-memory bytes objects like files
 import os
+from quixstreams import Application
+
+app = Application.Quix()
+
 
 # Define the current date
 current_date = datetime.now().strftime('%Y-%m-%d')
@@ -13,40 +17,33 @@ base_url = "https://www.regelleistung.net/apps/cpp-publisher/api/v1/download/ten
 # Build the full URL with the current date
 url = base_url.format(current_date)
 
-# Add additional value which is used for telegraf to make a proper measurement name
-measurement_name = "capacity_fcr_results"
+topic = app.topic(name=os.environ["output"], value_serializer='json')
 
-# Initialize Quix streaming client
-client = qx.QuixStreamingClient()
 
-# Open the output topic where to write data out
-# Open the output topic where to write data out
-topic_producer = client.get_topic_producer(topic_id_or_name = os.environ["output"])
+# strings for key and headers will be serialized to bytes by default
+i = 0
+with app.get_producer() as producer:
 
-# Create a stream for the data
-stream = topic_producer.create_stream()
-stream.properties.name = "Energy Data Stream"
+    try:
+        # Send a GET request to the URL and load the content directly into a DataFrame
+        response = requests.get(url)
+        if response.status_code == 200:
+            with io.BytesIO(response.content) as file:
+                df = pd.read_excel(file)
 
-try:
-    # Send a GET request to the URL and load the content directly into a DataFrame
-    response = requests.get(url)
-    if response.status_code == 200:
-        with io.BytesIO(response.content) as file:
-            df = pd.read_excel(file)
+                json = df.to_json(orient='records', lines=True)
+            
+                print(f'Data loaded successfully for {current_date}')
 
-            json = df.to_json(orient='records', lines=True)
-        
-            print(f'Data loaded successfully for {current_date}')
+                for row in json:
+                    producer.produce(topic.name, row, )
+                    
 
-            for row in json:
-                
+            # Iterate over the processed DataFrame and send each row as a time series data point
+        else:
+            print(f'File download failed. Status code: {response.status_code}')
+    except Exception as e:
+        print(f'An error occurred: {str(e)}')
 
-        # Iterate over the processed DataFrame and send each row as a time series data point
-    else:
-        print(f'File download failed. Status code: {response.status_code}')
-except Exception as e:
-    print(f'An error occurred: {str(e)}')
-
-# Closing the stream after sending all data points
-print("Closing stream")
-stream.close()
+    # Closing the stream after sending all data points
+    print("Closing stream")
