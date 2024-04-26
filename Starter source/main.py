feadb1b47b26633972cc7c3290893bd31910ae7c
Ricardo Kissinger <ricardo.kissinger+quix@juniz.com>
2024-04-26 14:07:46 +0000

    Edited 'main.py'

--- Starter source/main.py
+++ Starter source/main.py
@@ -67,7 +67,7 @@ with app.get_producer() as producer:
            
         print(f'Data loaded successfully for {current_date}')
 
-                for row in json.loads(json_str):
+               for row in json.loads(json_str):
                     print(row)
                     producer.produce(topic.name, json.dumps(row), str(row['PRODUCTNAME']))
                     
