ef161b10016c13f8ed553b6bd905577618064461
Tomas Neubauer <tomas+juniz@quix.io>
2024-04-26 16:29:42 +0000

    Edited 'main.py'

--- InfluxDB 3.0/main.py
+++ InfluxDB 3.0/main.py
@@ -12,7 +12,7 @@ from influxdb_client_3 import InfluxDBClient3
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-app = Application.Quix(consumer_group="influx-destination",
+app = Application.Quix(consumer_group="influx-destination-v1",
                        auto_offset_reset="earliest")
 
 input_topic = app.topic(os.environ["input"], value_deserializer=JSONDeserializer())
