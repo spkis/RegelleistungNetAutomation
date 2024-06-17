afacf53736d74696ce8629ef02502042ee0ff1b6
Ricardo Kissinger <ricardo.kissinger+quix@juniz.com>
2024-06-17 12:03:54 +0000

    Edited 'main.py'

--- InfluxDB 3.0/main.py
+++ InfluxDB 3.0/main.py
@@ -19,16 +19,20 @@ input_topic = app.topic(os.environ["input"], value_deserializer=JSONDeserializer
 
 # Read the environment variable and convert it to a dictionary
 tag_dict = ast.literal_eval(os.environ.get('INFLUXDB_TAG_COLUMNS', "{}"))
-tags = ['country']
-fields = ['timestamp', 'country', 'demand', 'price', 'deficit_surplus']
+#tags = ['country']
+#fields = ['timestamp', 'country', 'demand', 'price', 'deficit_surplus']
+tag_list = {tag: "" for tag in tag_dict}  # Create a dictionary from the list
+# Read the environment variable and convert it to a list
+tags_list = ast.literal_eval(os.environ.get('INFLUXDB_TAG_COLUMNS', "[]"))
+tag_dict_template = {tag: "" for tag in tags_list}  # Create a dictionary from the list
 
 # Read the environment variable for measurement name
 measurement_name = os.environ.get('INFLUXDB_MEASUREMENT_NAME', os.environ["input"])
 
 # Read the environment variable for the field(s) to get.
-# For multiple fields, use a list "['field1','field2']"
-#field_keys = os.environ.get("INFLUXDB_FIELD_KEYS", "['PRODUCTNAME','CROSSBORDER_SETTLEMENTCAPACITY_PRICE_[EUR/MW]','AUSTRIA_DEMAND_[MW]','AUSTRIA_SETTLEMENTCAPACITY_PRICE_[EUR/MW]','AUSTRIA_DEFICIT(-)_SURPLUS(+)_[MW]','BELGIUM_DEMAND_[MW]','BELGIUM_SETTLEMENTCAPACITY_PRICE_[EUR/MW]','BELGIUM_DEFICIT(-)_SURPLUS(+)_[MW]','DENMARK_DEMAND_[MW]','DENMARK_SETTLEMENTCAPACITY_PRICE_[EUR/MW]','DENMARK_DEFICIT(-)_SURPLUS(+)_[MW]','FRANCE_DEMAND_[MW]','FRANCE_SETTLEMENTCAPACITY_PRICE_[EUR/MW]','FRANCE_DEFICIT(-)_SURPLUS(+)_[MW]','GERMANY_DEMAND_[MW]','GERMANY_SETTLEMENTCAPACITY_PRICE_[EUR/MW]','GERMANY_DEFICIT(-)_SURPLUS(+)_[MW]','NETHERLANDS_DEMAND_[MW]','NETHERLANDS_SETTLEMENTCAPACITY_PRICE_[EUR/MW]','NETHERLANDS_DEFICIT(-)_SURPLUS(+)_[MW]','SLOVENIA_DEMAND_[MW]','SLOVENIA_SETTLEMENTCAPACITY_PRICE_[EUR/MW]','SLOVENIA_DEFICIT(-)_SURPLUS(+)_[MW]','SWITZERLAND_DEMAND_[MW]','SWITZERLAND_SETTLEMENTCAPACITY_PRICE_[EUR/MW]','SWITZERLAND_DEFICIT(-)_SURPLUS(+)_[MW]','CZECH_REPUBLIC_DEMAND_[MW]','CZECH_REPUBLIC_SETTLEMENTCAPACITY_PRICE_[EUR/MW]','CZECH_REPUBLIC_DEFICIT(-)_SURPLUS(+)_[MW]']")
-field_keys = ast.literal_eval(os.environ.get("INFLUXDB_FIELD_KEYS", "[]"))                                           
+# For multiple fields, use a list "['field1','field2']"   
+fields_list = ast.literal_eval(os.environ.get("INFLUXDB_FIELD_KEYS", "[]"))    
+field_dict_template = {field: "" for field in fields_list}  # Create a dictionary from the list                                    
 influx3_client = InfluxDBClient3(token=os.environ["INFLUXDB_TOKEN"],
                          host=os.environ["INFLUXDB_HOST"],
                          org=os.environ["INFLUXDB_ORG"],
@@ -41,11 +45,11 @@ def send_data_to_influx(message):
         # Extract the timestamp
         quixtime = message['timestamp']
 
-        # Extract the tags
-        tag_dict = {tag: message[tag] for tag in tags}
+        # Create a tag dictionary based on the template and populate it with values from the message
+        tag_dict = {tag: message[tag] for tag in tag_dict_template.keys()}
 
         # Extract the fields
-        field_dict = {field: message[field] for field in fields if field not in tags and field != 'timestamp'}
+        field_dict = {field: message[field] for field in field_dict_template if field not in tag_dict_template and field != 'timestamp'}
 
         logger.info(f"Using fields: {', '.join(field_dict.keys())}")
         logger.info(f"Using tags: {', '.join(tag_dict.keys())}")
