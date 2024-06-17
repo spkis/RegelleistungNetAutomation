2bf69d587d787b86c9209db98418da16a4bb6338
Ricardo Kissinger <ricardo.kissinger+quix@juniz.com>
2024-06-17 11:56:41 +0000

    Edited 'main.py'

--- InfluxDB 3.0/main.py
+++ InfluxDB 3.0/main.py
@@ -20,6 +20,8 @@ input_topic = app.topic(os.environ["input"], value_deserializer=JSONDeserializer
 # Read the environment variable and convert it to a dictionary
 tag_dict = ast.literal_eval(os.environ.get('INFLUXDB_TAG_COLUMNS', "{}"))
 tags = ['country']
+fields = ['timestamp', 'country', 'demand', 'price', 'deficit_surplus']
+
 # Read the environment variable for measurement name
 measurement_name = os.environ.get('INFLUXDB_MEASUREMENT_NAME', os.environ["input"])
 
